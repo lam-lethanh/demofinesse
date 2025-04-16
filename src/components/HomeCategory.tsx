9d8d8793842b316885953c6cb9e2f80f81b44403
Le Thanh Lam <lamlt@Mac-mini-cua-Le.local>
2025-04-17 00:12:48 +0700

    done

--- src/components/HomeCategory.tsx
+++ src/components/HomeCategory.tsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 
 function HomeCategory () {
     const categories = [
-        { label: "HOODIE", img: "src/assets/Cate_Hoodie.png", url: "/collections/hoodie" },
-        { label: "POLO SHIRT", img: "src/assets/Cate_Polo.png", url: "/collections/polo-shirt" },
-        { label: "T-SHIRT", img: "src/assets/Cate_TShirt.png", url: "/collections/t-shirt" },
-        { label: "HAWAIIAN SHIRT", img: "src/assets/Cate_Hawaiian_Shirt.png", url: "/collections/hawaiian-shirt" },
-        { label: "JERSEY", img: "src/assets/Cate_Jersey.png", url: "/collections/jersey" },
-        { label: "HOCKEY JERSEY", img: "src/assets/Cate_Hockey_Jersey.png", url: "/collections/hockey-jersey" },
-        { label: "LONG SLEEVE BUTTON", img: "src/assets/Cate_Long_Sleeve_Button.png", url: "/collections/long-sleeve-button" },
-        { label: "SKIRT", img: "src/assets/Cate_Skirt.png", url: "/collections/skirt" },
-        { label: "SHOES", img: "src/assets/Cate_Shoes.png", url: "/collections/shoes" },
-        { label: "BEDDING SET", img: "src/assets/Cate_Bedding_Set.png", url: "/collections/bedding-set" },
-        { label: "LUGGAGE COVER", img: "src/assets/Cate_Luggage_Cover.png", url: "/collections/luggage-cover" },
-        { label: "TUMBLER", img: "src/assets/Cate_Tumbler.png", url: "/collections/tumbler" }
+        { label: "HOODIE", img: "img/Cate_Hoodie.png", url: "/collections/hoodie" },
+        { label: "POLO SHIRT", img: "img/Cate_Polo.png", url: "/collections/polo-shirt" },
+        { label: "T-SHIRT", img: "img/Cate_TShirt.png", url: "/collections/t-shirt" },
+        { label: "HAWAIIAN SHIRT", img: "img/Cate_Hawaiian_Shirt.png", url: "/collections/hawaiian-shirt" },
+        { label: "JERSEY", img: "img/Cate_Jersey.png", url: "/collections/jersey" },
+        { label: "HOCKEY JERSEY", img: "img/Cate_Hockey_Jersey.png", url: "/collections/hockey-jersey" },
+        { label: "LONG SLEEVE BUTTON", img: "img/Cate_Long_Sleeve_Button.png", url: "/collections/long-sleeve-button" },
+        { label: "SKIRT", img: "img/Cate_Skirt.png", url: "/collections/skirt" },
+        { label: "SHOES", img: "img/Cate_Shoes.png", url: "/collections/shoes" },
+        { label: "BEDDING SET", img: "img/Cate_Bedding_Set.png", url: "/collections/bedding-set" },
+        { label: "LUGGAGE COVER", img: "img/Cate_Luggage_Cover.png", url: "/collections/luggage-cover" },
+        { label: "TUMBLER", img: "img/Cate_Tumbler.png", url: "/collections/tumbler" }
       ];
       
 
